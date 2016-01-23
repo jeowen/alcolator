19cd8c51c5e349f5366d15a26e62cbad61de214e
jason.owen@va.gov <jason.owen@va.gov>
2016-01-23 15:35:20 -0800

    Created v1.1 of alcolator, working

---
+++ alcolator v1.0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  alcolator v1.0
+//
+//  Created by Jason Owen on 1/23/16.
+//  Copyright © 2016 Jason Owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
