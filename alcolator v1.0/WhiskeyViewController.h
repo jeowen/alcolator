c22753bfdc5535786fcc3b8f041d9c4ac0d0867c
jason.owen@va.gov <jason.owen@va.gov>
2016-01-26 06:43:46 -0800

    updated to include whiskey view controller, hopefully

---
+++ alcolator v1.0/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  alcolator v1.0
+//
+//  Created by Jason Owen on 1/26/16.
+//  Copyright © 2016 Jason Owen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
