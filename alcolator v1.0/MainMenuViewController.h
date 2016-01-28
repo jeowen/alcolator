fb8ab65e712b70ee24010cdf6ae21b983728c71a
jason.owen@va.gov <jason.owen@va.gov>
2016-01-27 22:06:41 -0800

    added navigation controller

---
+++ alcolator v1.0/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  alcolator v1.0
+//
+//  Created by Jason Owen on 1/27/16.
+//  Copyright © 2016 Jason Owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
